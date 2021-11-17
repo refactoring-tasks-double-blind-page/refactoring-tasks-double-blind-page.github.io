a33cd093b452920b086174c679c47e2b1f99500b
user-blind <78178970+user-blind@users.noreply.github.com>
2021-11-17 16:11:40 -0300

    deploy

---
+++ precache-manifest.b66aa9fe0c890001177b7f5924466a68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69b379f38e6813983ae17022fe8f8788",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60423b21c2178bd5ac81",
+    "url": "/static/css/main.da1d791a.chunk.css"
+  },
+  {
+    "revision": "47e532add054f47e9170",
+    "url": "/static/js/2.2902fd4f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.2902fd4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60423b21c2178bd5ac81",
+    "url": "/static/js/main.262f2df1.chunk.js"
+  },
+  {
+    "revision": "cd6e1fa99533c3033489",
+    "url": "/static/js/runtime-main.f41738d6.js"
+  }
+]);
